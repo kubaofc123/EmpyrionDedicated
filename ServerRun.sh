d8f3ac90a0633e9d10986ece090aa1a1821e4f75
Jakub Brzyski <kuba@2222.pl>
2024-04-04 11:39:08 +0200

    updated reforged eden version

--- ServerRun.sh
+++ ServerRun.sh
@@ -12,7 +12,7 @@ git pull
 ssh-add -D
 
 # Ask which save to launch
-read -p "Select your game save: DedGame(1), Scenariusz2(2), Shared-1(3): " selected_save
+read -p "Select your game save: DedGame(1), Scenariusz2(2), Shared-1(3), Shared-2(4): " selected_save
 read -p "Update server? (y/n): " update_server    
 if [ "$update_server" == "y" ]; then 
     read -p "Validate after update? (y/n):  " validate_update
@@ -31,3 +31,4 @@ cd "D:\EmpyrionDedicatedServer\steamapps\common\Empyrion - Dedicated Server"
 if [ "$selected_save" = 1 ]; then ./MyEmpyrionDedicatedScen1.cmd; fi
 if [ "$selected_save" = 2 ]; then ./MyEmpyrionDedicatedScen2.cmd; fi
 if [ "$selected_save" = 3 ]; then ./MyEmpyrionDedicatedScen3.cmd; fi
+if [ "$selected_save" = 4 ]; then ./MyEmpyrionDedicatedScen4.cmd; fi
---
+++ steamapps/common/Empyrion - Dedicated Server/MyEmpyrionDedicatedScen4.cmd
@@ -0,0 +1,2 @@
+@echo off
+EmpyrionLauncher -startDediWithGfx -dedicated MyDedicatedConfigScen4.yaml
